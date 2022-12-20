56979d0eb2895a7c31d0904189f7beadaea8d576
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-12-21 00:49:44 +0900

    add image

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <p>hoge</p>
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
+    <img src="https://firebasestorage.googleapis.com/v0/b/yuuukizm-pro.appspot.com/o/apparel%2Fapparel-10-min.png?alt=media&token=fdb588bd-da8b-485d-8e42-5343245c2c66">
   </div>
 </template>
