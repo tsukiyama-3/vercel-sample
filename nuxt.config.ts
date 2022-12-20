914c5eb5bcbe1fe5bc6d93ee0ae1a89fdcc402db
tsukiyama-3 <kohei.tsukiyama@yourmystar.jp>
2022-12-21 01:00:07 +0900

    add meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      title: "vercel sample",
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    }
+  }
 })
